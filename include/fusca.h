771510e98b0c82fa4034247b9252444b2d455915
Yuriy Romanenko <yuriy@lytro.com>
2016-10-23 08:47:24 -0700

    Added rudimentary implementation
    Change-Id: I99761bb978c0a961e868d104065b30aacd9dc15b

--- include/fusca.h
+++ include/fusca.h
@@ -2,29 +2,56 @@
 
 #include <type_traits>
 
-
-template<typename slow_clock_t, typename fast_clock_t>
-class fusca
+namespace fusca
 {
-    typedef typename std::result_of<slow_clock_t()>::type slow_val_t;
-    typedef typename std::result_of<fast_clock_t()>::type fast_val_t;
+  template<typename slow_clock_t, typename fast_clock_t>
+  class fusca
+  {
+      typedef typename std::result_of<slow_clock_t()>::type slow_val_t;
+      typedef typename std::result_of<fast_clock_t()>::type fast_val_t;
+      typedef decltype(std::declval<slow_clock_t>()() - std::declval<slow_clock_t>()()) slow_dur_t;
+      typedef decltype(std::declval<fast_clock_t>()() - std::declval<fast_clock_t>()()) fast_dur_t;
 
-    slow_clock_t m_sc;
-    fast_clock_t m_fc;
-  public:
-    fusca(slow_clock_t sc, fast_clock_t fc) : m_sc(sc), m_fc(fc)
-    {
+      slow_clock_t m_sc;
+      fast_clock_t m_fc;
 
-    }
+      slow_val_t m_sstart;
+      fast_val_t m_fstart;
 
-    inline slow_val_t operator()()
-    {
-      return m_sc();
-    }
-};
+      slow_dur_t m_speriod;
+      fast_dur_t m_fperiod;
 
-template<typename slow_clock_t, typename fast_clock_t>
-fusca<slow_clock_t, fast_clock_t> make_fusca(slow_clock_t sc, fast_clock_t fc)
-{
-  return fusca<slow_clock_t, fast_clock_t>(sc,fc);
+      inline slow_val_t estimate() const
+      {
+        fast_val_t fnow = m_fc();
+
+        return m_sstart + m_speriod * (fnow - m_fstart) / m_fperiod;
+      }
+    public:
+      fusca(slow_clock_t sc, fast_clock_t fc) : m_sc(sc), m_fc(fc)
+      {
+        slow_val_t st1 = m_sc();
+        fast_val_t ft1 = m_fc();
+        slow_val_t st2 = m_sc();
+        fast_val_t ft2 = m_fc();
+
+        m_fperiod = ft2 - ft1;
+        m_speriod = st2 - st1;
+
+        m_sstart = st2;
+        m_fstart = ft2;
+      }
+
+      inline slow_val_t operator()()
+      {
+        return estimate();
+      }
+  };
+
+  template<typename slow_clock_t, typename fast_clock_t>
+  fusca<slow_clock_t, fast_clock_t> make(slow_clock_t sc, fast_clock_t fc)
+  {
+    return fusca<slow_clock_t, fast_clock_t>(sc,fc);
+  }
+
 }
--- test/test_fusca.cpp
+++ test/test_fusca.cpp
@@ -5,14 +5,25 @@
 
 int main(int argc, char *argv[])
 {
-  auto tscclock = []{ return __rdtsc(); };
+  auto fastclock = []{ return __rdtsc(); };
   auto slowclock = [] { return std::chrono::high_resolution_clock::now(); };
-  auto adapter = make_fusca(slowclock, tscclock);
+  auto adapter = fusca::make(slowclock, fastclock);
 
   for(int i = 0; i < 100; i++)
   {
-    auto delta = adapter() - slowclock();
-    printf("Delta: %llu us\n", std::chrono::duration_cast<std::chrono::milliseconds>(delta).count());
+    auto st1 = slowclock();
+    auto st2 = slowclock();
+    auto ft1 = fastclock();
+    auto ft2 = fastclock();
+    auto at1 = adapter();
+    auto at2 = slowclock();
+    auto fdel = ft2 - ft1;
+    auto sdel = st2 - st1;
+    auto adel = at2 - at1;
+    printf("Fast Delta: %lld ticks Slow Delta %.4f us Adapter to Slow Delta: %.4f us\n",
+      fdel,
+      std::chrono::duration_cast<std::chrono::nanoseconds>(sdel).count()/1000.0,
+      std::chrono::duration_cast<std::chrono::nanoseconds>(adel).count()/1000.0);
   }
 
   return 0;
